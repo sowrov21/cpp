4352ea2c929927766d276232c03d5dfbc482ccd8
sowrov <sowrov.it@gmail.com>
2021-01-27 04:12:36 +0600

    pattern added

--- Pattern/main.cpp
+++ Pattern/main.cpp
@@ -1,29 +1,49 @@
 #include <iostream>
-
+#include <fstream>
 using namespace std;
 
 int main()
 {
+
    /*
+     ***
+     ***
+     ***
+     ***
+   */
+   /*char chr='*';
+
+   for(int i=0; i<4;i++)
+   {
+      for(int j=0;j<3;j++)
+       {
+         cout<<chr;
+       }
+       cout<<"\n";
+
+   }*/
+
+   //cout << "Hello world!" << endl;
+
+ /*     char chr='*';
+
+   for(int i=0; i<=6;i++)
+   {
+      for(int j=0;j<i;j++)
+       {
+         cout<<chr;
+       }
+       cout<<"\n";
+
+   }
+*/
+
+
+/*
    *******
     *****
      ***
       *
-
-   */
-
-
-   char chr='*';
-
-   for(int i=0; i<=7;i++)
-   {
-      for(int j=0;j<=4;j++)
-       {
-         cout<<chr<<endl;
-       }
-
-   }
-
-   //cout << "Hello world!" << endl;
+*/
     return 0;
 }
