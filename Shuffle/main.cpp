eec9513898fccdf1bee1114c5f0cab85a9532ec0
Sowrov21 <sowrov.it@gmail.com>
2021-03-26 08:56:41 +0600

    shuffle added

---
+++ Shuffle/main.cpp
@@ -0,0 +1,22 @@
+// shuffle algorithm example
+#include <iostream>     // std::cout
+#include <algorithm>    // std::shuffle
+#include <array>        // std::array
+#include <random>       // std::default_random_engine
+#include <chrono>       // std::chrono::system_clock
+//using namespace std;
+
+int main () {
+  std::array<int,10> foo {1,2,3,4,5,6,7,8,9,10};
+
+  // obtain a time-based seed:
+  unsigned seed = std::chrono::system_clock::now().time_since_epoch().count();
+
+  shuffle (foo.begin(), foo.end(), std::default_random_engine(seed));
+
+  std::cout << "shuffled elements:";
+  for (int& x: foo) std::cout << ' ' << x;
+  std::cout << '\n';
+
+  return 0;
+}
