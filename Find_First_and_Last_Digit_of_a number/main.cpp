44dd382dcfbcd97a34b13f48f36476d636b771a7
Sowrov21 <sowrov.it@gmail.com>
2021-04-10 00:44:19 +0600

    Find Last Digit Done

---
+++ Find_First_and_Last_Digit_of_a number/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <math.h>
+
+using namespace std;
+
+int main()
+{
+    int a=2356,last_dgt=0;
+    last_dgt = a%10;
+
+    cout<<"Last Digit of "<<a<<" is "<<last_dgt<<endl;
+
+    return 0;
+}
