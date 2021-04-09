05ec4063c3e9ffa9d2380c9f07d8f990e3d8357b
Sowrov21 <sowrov.it@gmail.com>
2021-04-10 00:47:52 +0600

    Find First Digit Done

--- Find_First_and_Last_Digit_of_a number/main.cpp
+++ Find_First_and_Last_Digit_of_a number/main.cpp
@@ -5,10 +5,16 @@ using namespace std;
 
 int main()
 {
-    int a=2356,last_dgt=0;
+    int a=2356,last_dgt=0,first_dgt=0,digit=0;
     last_dgt = a%10;
 
+    digit = log10(a);
+    //cout<<digit<<endl;
+
+    int res = a/pow(10,digit); //pow(base,exponent)2^3=8
+
     cout<<"Last Digit of "<<a<<" is "<<last_dgt<<endl;
+    cout<<"First Digit of "<<a<<" is "<<res<<endl;
 
     return 0;
 }
